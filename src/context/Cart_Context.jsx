4f33aa9fd5d92fe938783532953389d1f0b3c079
sabhaya meet <sabhayameet76980@gmail.com>
2025-04-29 14:19:07 +0530

    add to cart show data and remove functionality

--- src/context/Cart_Context.jsx
+++ src/context/Cart_Context.jsx
@@ -1,10 +1,22 @@
 import React, { createContext, useReducer } from "react";
 import reducer from "../reducer/CartReducer";
+import { useEffect } from "react";
 
 export const CartContext = createContext();
 
+const getLocalCartData = () => {
+  let newCartData = localStorage.getItem("cartData");
+
+  if (newCartData == []) {
+    return [];
+  } else {
+    return JSON.parse(newCartData);
+  }
+};
+
 const intialState = {
-  cart: [],
+  //   cart: [],
+  cart: getLocalCartData(),
   total_item: "",
   total_amount: "",
   shipping_fee: 50000,
@@ -16,8 +28,26 @@ const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
+
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
+
+  //   to the clear cart
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  //   to add the data in localstorage
+
+  useEffect(() => {
+    localStorage.setItem("cartData", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/reducer/CartReducer.jsx
+++ src/reducer/CartReducer.jsx
@@ -3,7 +3,41 @@ import React from "react";
 const CartReducer = (state, action) => {
   if (action.type === "ADD_TO_CART") {
     let { id, color, amount, product } = action.payload;
-    console.log(product);
+
+    let cartProduct;
+
+    cartProduct = {
+      id: id + color,
+      name: product.name,
+      color,
+      amount,
+      image: product.image[0].url,
+      price: product.price,
+      max: product.stock,
+    };
+
+    return {
+      ...state,
+      cart: [...state.cart, cartProduct],
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    let updatedCart = state.cart.filter(
+      (curElem) => curElem.id !== action.payload
+    );
+    return {
+      ...state,
+      cart: updatedCart,
+    };
+  }
+
+  //   to empty clear the cart
+  if (action.type === "CLEAR_CART") {
+    return {
+      ...state,
+      cart: [],
+    };
   }
 
   return state;
